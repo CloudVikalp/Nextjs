b4cb63c7dfed9cf9088d57267dbe9f67c63d500b
CloudVikalp <vikalp.shandilya@cloudactivelabs.in>
2023-04-24 17:44:46 +0530

    admin dashboard
    1.make home route
    2.make sidebar
    3.make order components

--- tailwindcss/pages/_app.tsx
+++ tailwindcss/pages/_app.tsx
@@ -1,6 +1,11 @@
+import Siderbar from '@/components/Siderbar'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <Siderbar>
+    <Component {...pageProps} />
+  </Siderbar>
+  )
 }
