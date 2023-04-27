791436cb7c60e5fcdb91fa676fab95917b52d9c2
CloudVikalp <vikalp.shandilya@cloudactivelabs.in>
2023-04-27 16:49:38 +0530

    new project with atilwind css

---
+++ dashboard_2-main/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Header from './components/header/Header';
+import NavBar from './components/navbar/NavBar';
+import LeftPart from './components/leftPart/LeftPart';
+import RightPart from './components/rightPart/RightPart';
+
+function App() {
+  return (
+    <div className="App overflow-y-hidden ">
+     <Header/>
+     <div className='w-full min-h-[90vh] grid grid-cols-12'>
+     <NavBar/>
+     <div className='grid grid-cols-1 xl:grid-cols-5 col-span-10 w-full'>
+      <LeftPart/>
+      <RightPart/>
+     </div>
+     </div>
+    </div>
+  );
+}
+
+export default App;
